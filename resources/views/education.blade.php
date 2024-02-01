896b7111e80c09e3eee3f81134f50240752d1e67
AlvinaKrisendi <alvina.manggar@gmail.com>
2024-02-01 15:28:50 +0700

    upd edu

---
+++ resources/views/education.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <link href="Style/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amaranth&family=Patrick+Hand&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <style>
+        * {
+            font-family: 'Quicksand', 'Amaranth', cursive, Arial, sans-serif;
+        }
+        
+    </style>
+
+    <link rel="website icon" type="png" href="../Image/logo.jpg"> 
+        <title>EqualizeMe-Education</title>
+
+    
+</head>
+<body>
+    <h1>abc</h1>
+    </body>
+    </html>
