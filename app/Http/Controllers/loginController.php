20f41edc7e94c4470df59fd8af10deb6713eead5
shandez14 <127479866+shandez14@users.noreply.github.com>
2024-03-04 20:17:19 +0700

    pull dari evan

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -1,14 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
-use Illuminate\Http\Request;
 
-class loginController extends Controller
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class LoginController extends Controller
 {
-    public function index(){
-        return view('login', [
-            "akuns" => User::find(2)
+    public function authenticate(Request $request){
+        $credentials = $request->validate([
+            'nik' => 'required|max:16|min:16',
+            'password' => 'required|min:5|max:255',
         ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/home');
+        }
+        
+        return back()->with('loginError', 'wrong input nik or password!');
+
+        // Session::flash('status', 'failed');
+        // Session::flash('message', 'wrong input nik or password');
+
+        // return redirect('/login');
+
     }
+
+    public function index() {
+        return view("login");
+    }
+
 }
